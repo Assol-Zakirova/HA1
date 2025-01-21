96229480beb17cf69fe297d4043088ceea9812d4
Assol-Zakirova <zakirovaassol@gmail.com>
2025-01-21 13:38:29 +0600

    Create myfamily.py

---
+++ myfamily.py
@@ -0,0 +1,7 @@
+myfamily = ("mother", "father", "sister", "brother", "sister")
+print(myfamily)
+print(type(myfamily))
+print(myfamily[2])
+print(myfamily[-1])
+# we cannot use 'append' in terms of tuples
+# we cannot use 'pop' method in terms of tuples
